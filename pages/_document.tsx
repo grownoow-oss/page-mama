f8a315bc0426dd869257ea0e261693775c74ea10
@grownoow-oss <grownoow@gmail.com>
2026-04-24 03:24:04 +0600

    feat: add brand favicons

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -16,6 +16,10 @@ export default function Document() {
           href="https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@400;500;600;700&family=Hind+Siliguri:wght@300;400;500;600;700&family=Outfit:wght@400;500;600;700;800;900&display=swap"
           rel="stylesheet"
         />
+        {/* Favicons */}
+        <link rel="icon" type="image/png" href="/favicon.png" />
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+
         {/* FontAwesome for Icons */}
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" />
       </Head>
